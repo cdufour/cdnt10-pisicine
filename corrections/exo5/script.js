a9fa8660620347b1effd9b7662a18f8f2109daaa
Christophe DUFOUR <opusidea@gmail.com>
2020-03-17 10:22:02 +0100

    exo 5

---
+++ corrections/exo5/script.js
@@ -0,0 +1,12 @@
+// exo 5
+
+(function() {
+
+// Constants
+const QUOTES = [
+    { body: "Tout vient au point à qui sait attendre", topic: "Patience" },
+    { body: "Ad astra per aspera", topic: "Courage" },
+    { body: "An apple a day keeps the doctor away", topic: "Santé" },
+];
+
+})()
