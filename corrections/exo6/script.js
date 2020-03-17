f2eff8ec692ac210e7b3cb70fbce0a73cf0acc46
Christophe DUFOUR <opusidea@gmail.com>
2020-03-17 16:07:55 +0100

    exo 7

---
+++ corrections/exo6/script.js
@@ -0,0 +1,53 @@
+// exo 6
+
+(function() {
+
+// Constants & globals
+let players = null;
+
+// Dom targetting
+let tabPlayers     = document.querySelector('#tabPlayers tbody');
+let txtSearch      = document.getElementById('txtSearch');
+
+// Events
+txtSearch.addEventListener('keyup', filterPlayers);
+
+// Functions
+function getPlayers() {
+    fetch('players.json')
+        .then(res => res.json())
+        .then(res => {
+            players = res;
+            buildTable(players);
+        })
+}
+
+function buildTable(players) {
+    tabPlayers.innerHTML = '';
+    players.forEach(p => {
+        let tr = document.createElement('tr');
+        tr.innerHTML = `
+            <td>${p.firstname}</td>
+            <td>${p.lastname}</td>
+            <td>${p.team}</td>
+            <td class="position">${p.position}</td>
+        `;
+        tabPlayers.appendChild(tr);
+    })
+}
+
+function filterPlayers(e) {
+    let input = e.target.value;
+    if (input.length >= 2) {
+        let filteredPlayers = players.filter(
+            p => p.lastname.indexOf(input) != -1 || p.team.indexOf(input) != -1);
+        buildTable(filteredPlayers);
+    } else {
+        buildTable(players);
+    }
+}
+
+// Init
+getPlayers();
+
+})()
